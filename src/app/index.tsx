ceb513422c99e49738aa22a7caa9f85f58f15269
Rejaur Rahman <rejaur-rahman@hotmail.co.uk>
2024-04-09 01:44:48 +0100

    ADD: camera view setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,10 +7,18 @@ import {
   View
 } from "react-native"
 
-import { useCameraPermission } from "react-native-vision-camera"
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission
+} from "react-native-vision-camera"
 
 export default function HomeScreen() {
   const { hasPermission, requestPermission } = useCameraPermission()
+
+  const device = useCameraDevice("back", {
+    physicalDevices: ["ultra-wide-angle-camera"]
+  })
 
   useEffect(() => {
     if (!hasPermission) {
@@ -22,21 +30,20 @@ export default function HomeScreen() {
     return <ActivityIndicator />
   }
 
+  if (!device) {
+    return <Text>Camera device not found</Text>
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={{ flex: 1 }}>
       <Stack.Screen
         options={{ headerShown: false }}
       />
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Camera
+        device={device}
+        isActive={true}
+        style={StyleSheet.absoluteFill}
+      />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    backgroundColor: "#fff",
-    flex: 1,
-    justifyContent: "center"
-  }
-})
