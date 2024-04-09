327fb1bfddca9d624cf42e3c1225f5ac76499dac
Rejaur Rahman <rejaur-rahman@hotmail.co.uk>
2024-04-09 01:32:02 +0100

    UPDATE: request and handle camera permissions

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,27 @@
 import { Stack } from "expo-router"
+import React, { useEffect } from "react"
 import {
+  ActivityIndicator,
   StyleSheet,
   Text,
   View
 } from "react-native"
 
+import { useCameraPermission } from "react-native-vision-camera"
+
 export default function HomeScreen() {
+  const { hasPermission, requestPermission } = useCameraPermission()
+
+  useEffect(() => {
+    if (!hasPermission) {
+      requestPermission();
+    }
+  }, [hasPermission])
+
+  if (!hasPermission) {
+    return <ActivityIndicator />
+  }
+
   return (
     <View style={styles.container}>
       <Stack.Screen
